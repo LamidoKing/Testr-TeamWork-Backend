5cc8dc19b72724b8d6f9eb051cffec7e2fc07780
lamido <kinglamido@gmail.com>
2019-10-29 20:49:13 +0100

    added and config jasmine test

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
-const express = require("express")
+const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 app.use(bodyParser.json());
 
-app.use('/api/v1', (req, res, next) => {
-    const massage = {
-        message: "welcome to Teamwork"
-    }
-    res.status(200).json(massage);
+app.use('/api/v1', (req, res) => {
+  res.send('Hello World');
 });
-module.exports = app
+module.exports = app;
